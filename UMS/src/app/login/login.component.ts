8203a43c7357fad3a24696504b4fa87a1dcfb882
HIDRAN ARIAS <hidran.arias@eng.it>
2018-02-07 08:43:44 +0100

    add login form

--- UMS/src/app/login/login.component.ts
+++ UMS/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( private  auth: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
-
+  signIn(form: NgForm) {
+      if(!form.valid){
+          return false;
+      }
+  let result =  this.auth.signIn(form.value.email, form.value.password);
+      if(result){
+          this.router.navigate([''])
+      }
+  }
 }
--- UMS/src/app/services/auth.service.ts
+++ UMS/src/app/services/auth.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthService {
-  private isUserLogged = true;
+  private isUserLogged = false;
   constructor() { }
   
   isUserLoggedIn() {
+      this.isUserLogged = !!localStorage.getItem('token');
      return this.isUserLogged;
   }
-  signIn(email: String, password: string){
+  signIn(email: string, password: string){
+     
+      localStorage.setItem('token', email);
+      return true;
       
   }
     signUp(username:string, email: String, password: string){
 
     }
     logout() {
+        localStorage.removeItem('token');
       this.isUserLogged = false;
     }
 }
