a179685cf30ddef8119fc1f83dec8db3cb0b033a
HIDRAN ARIAS <hidran.arias@eng.it>
2018-02-16 23:58:36 +0100

    add jwt-auth package

--- LARAAPI/app/User.php
+++ LARAAPI/app/User.php
@@ -4,8 +4,8 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -26,4 +26,20 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();  
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims(){
+        return [
+            'name' => $this->name,
+            'email'=> $this->email
+            ];
+    }
 }
