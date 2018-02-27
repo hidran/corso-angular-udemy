a6b0eeafe86d9ec5229810b7cd73629950ed2e21
HIDRAN ARIAS <hidran.arias@eng.it>
2018-02-27 08:53:21 +0100

    add token to user service requests

--- LARAAPI/app/Http/Controllers/UsersController.php
+++ LARAAPI/app/Http/Controllers/UsersController.php
@@ -10,7 +10,7 @@ class UsersController extends Controller
 {
     public function __construct()
     {
-       
+       $this->middleware('auth:api');
     }
 
     /**
--- UMS/src/app/services/auth.service.ts
+++ UMS/src/app/services/auth.service.ts
@@ -1,29 +1,55 @@
 import {EventEmitter, Injectable, Output} from '@angular/core';
 import {User} from "../classes/User";
-
+import {HttpClient, HttpErrorResponse, HttpHeaderResponse} from "@angular/common/http";
+import Any = jasmine.Any;
+ interface Jwt {
+     access_token: string,
+     token_type: string
+     expires_in : number,
+     user_name: string,
+     email: string
+ }
 @Injectable()
 export class AuthService {
     private isUserLogged = false;
     @Output() usersignedin = new EventEmitter<User>()
     @Output() usersignedup = new EventEmitter<User>()
     @Output() userlogout = new EventEmitter()
-
-    constructor() {
+    private APIAUTHURL = 'http://localhost:8000/api/auth/';
+    constructor(private http: HttpClient) {
     }
 
     isUserLoggedIn() {
+       
         this.isUserLogged = !!localStorage.getItem('token');
         return this.isUserLogged;
     }
 
     signIn(email: string, password: string) {
-
-        localStorage.setItem('token', email);
-        let user = new User();
-        user.name = 'Test';
-        user.email = email;
-        this.usersignedin.emit(user);
-        return true;
+        
+        this.http.post(this.APIAUTHURL + 'login',
+            {
+                email: email,
+                password: password
+            }
+            ).subscribe(
+            (payload: Jwt) => {
+                localStorage.setItem('token', payload.access_token);
+                console.log(payload)
+                localStorage.setItem('user' , JSON.stringify(payload));
+                let user = new User();
+                user.name = payload.user_name;
+                user.email = payload.email;
+                this.usersignedin.emit(user);
+                return true;
+                   
+        } ,
+            (httpResp: HttpErrorResponse) => {
+              
+               alert(httpResp.message)
+            }
+        )
+       
 
     }
 
@@ -39,7 +65,20 @@ export class AuthService {
 
     logout() {
         localStorage.removeItem('token');
-        this.userlogout.emit();
+        localStorage.removeItem('user');
+            this.userlogout.emit();
         this.isUserLogged = false;
+    }
+    getUser(): User {
+        const data = JSON.parse(localStorage.getItem('user'));
+        let user = new User();
+        if(data){
+            user.name = data['user_name'];
+            user.email = data['email'];
+        }
+       return user; 
+    }
+    getToken() {
+        return localStorage.getItem('token');
     }
 }
